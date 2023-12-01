64c98e58c41890a2e50ce3e581e55c556b66008a
scixing <whs11998@163.com>
2023-12-01 17:42:55 +0800

    数据库迁移

---
+++ PokemonisshoniZ/PokemonisshoniZ/Migrations/20231201094247_addremark.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PokemonisshoniZ.Migrations
+{
+    /// <inheritdoc />
+    public partial class addremark : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Remark",
+                table: "PokeTeams",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Remark",
+                table: "PokeTeams");
+        }
+    }
+}
