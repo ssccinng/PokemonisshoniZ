db138c2a4dd6a44b5b6618de0a1a5b1fe08fd44b
scixing <whs11998@163.com>
2023-12-01 09:32:53 +0800

    数据库迁移文件

---
+++ PokemonisshoniZ/PokemonisshoniZ/Migrations/20231201013141_addteam.cs
@@ -0,0 +1,159 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PokemonisshoniZ.Migrations
+{
+    /// <inheritdoc />
+    public partial class addteam : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PSUsername_AspNetUsers_UserId",
+                table: "PSUsername");
+
+            migrationBuilder.UpdateData(
+                table: "PSUsername",
+                keyColumn: "UserId",
+                keyValue: null,
+                column: "UserId",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "PSUsername",
+                type: "varchar(255)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "PCLPokemonBoxes",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    UserId = table.Column<string>(type: "varchar(255)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Name = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    BoxIdx = table.Column<int>(type: "int", nullable: false),
+                    PCLPokemonIds = table.Column<string>(type: "json", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PCLPokemonBoxes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PCLPokemonBoxes_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "PCLPokemons",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    PokeId = table.Column<long>(type: "bigint", nullable: false),
+                    Tags = table.Column<string>(type: "json", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    PSText = table.Column<string>(type: "nvarchar(1000)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PCLPokemons", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "PokeTeams",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    UserId = table.Column<string>(type: "varchar(255)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    PSText = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    PokemonIds = table.Column<string>(type: "json", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Islock = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    Tags = table.Column<string>(type: "json", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PokeTeams", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PokeTeams_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PCLPokemonBoxes_UserId",
+                table: "PCLPokemonBoxes",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PokeTeams_UserId",
+                table: "PokeTeams",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PSUsername_AspNetUsers_UserId",
+                table: "PSUsername",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PSUsername_AspNetUsers_UserId",
+                table: "PSUsername");
+
+            migrationBuilder.DropTable(
+                name: "PCLPokemonBoxes");
+
+            migrationBuilder.DropTable(
+                name: "PCLPokemons");
+
+            migrationBuilder.DropTable(
+                name: "PokeTeams");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "PSUsername",
+                type: "varchar(255)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PSUsername_AspNetUsers_UserId",
+                table: "PSUsername",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
