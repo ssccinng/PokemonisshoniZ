abeaaeda47f2bf133abfe6cc18856e3013c3ffa1
scixing <whs11998@163.com>
2023-12-02 23:47:56 +0800

    数据库迁移

---
+++ PokemonisshoniZ/PokemonisshoniZ/Migrations/20231202154741_addTeamName.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PokemonisshoniZ.Migrations
+{
+    /// <inheritdoc />
+    public partial class addTeamName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "PokeTeams",
+                type: "nvarchar(50)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TeamType",
+                table: "PokeTeams",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "PokeTeams");
+
+            migrationBuilder.DropColumn(
+                name: "TeamType",
+                table: "PokeTeams");
+        }
+    }
+}
