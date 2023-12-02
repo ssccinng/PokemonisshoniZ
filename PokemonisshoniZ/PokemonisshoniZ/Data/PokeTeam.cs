eafb212afa7194ff72dccdbd2a7a9d6ecf84741a
scixing <whs11998@163.com>
2023-12-02 23:45:22 +0800

    add teamlist

--- PokemonisshoniZ/PokemonisshoniZ/Data/PokeTeam.cs
+++ PokemonisshoniZ/PokemonisshoniZ/Data/PokeTeam.cs
@@ -1,10 +1,19 @@
-﻿namespace PokemonisshoniZ.Data
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PokemonisshoniZ.Data
 {
     public class PokeTeam
     {
+
+
         public int Id { get; set; }
         public ApplicationUser User { get; set; }
         public string UserId { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Name { get; set; } = string.Empty;
+
+        public TeamType TeamType { get; set; }
 
         public string PSText { get; set; } = string.Empty;
 
@@ -17,4 +26,11 @@
         /// </summary>
         public List<string> Tags { get; set; } = new List<string>();
     }
+
+    public enum TeamType
+    {
+        Private,
+        Public
+    }
+
 }
