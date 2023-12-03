e7cff9a8ec027ab10ebebd0138fc62dac42b25b4
scixing <whs11998@163.com>
2023-12-03 15:15:22 +0800

    迁移

---
+++ PokemonisshoniZ/PokemonisshoniZ/Migrations/20231203071509_addTeamCon.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PokemonisshoniZ.Migrations
+{
+    /// <inheritdoc />
+    public partial class addTeamCon : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ConcurrencyToken",
+                table: "PokeTeams",
+                type: "timestamp(6)",
+                rowVersion: true,
+                nullable: false)
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.ComputedColumn);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConcurrencyToken",
+                table: "PokeTeams");
+        }
+    }
+}
