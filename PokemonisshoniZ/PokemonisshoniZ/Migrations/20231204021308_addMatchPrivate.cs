ff21a0c06507a61277cc963bcbcd2c028f69d3e0
scixing <whs11998@163.com>
2023-12-04 10:14:33 +0800

    数据库迁移

---
+++ PokemonisshoniZ/PokemonisshoniZ/Migrations/20231204021308_addMatchPrivate.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PokemonisshoniZ.Migrations
+{
+    /// <inheritdoc />
+    public partial class addMatchPrivate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPrivate",
+                table: "PCLMatches",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPrivate",
+                table: "PCLMatches");
+        }
+    }
+}
